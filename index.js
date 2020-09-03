c758376c49c00161fb19eb8344894ab3074d8d17
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-03 14:50:17 -0400

    app

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
