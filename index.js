dfd231737acac41eba9ad77584bf815a1c396c03
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-03 15:19:10 -0400

    fierce fortress

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log('listening on *:3000');
 });
