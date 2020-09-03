fdffd28891b8e8975107c46fb73220429a8132b1
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-03 17:47:42 -0400

    whoops

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+// function generateRandomColor() {
+//     var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+//     return randomColor;
+//     //random color will be freshly served
+// }
+// document.body.style.backgroundColor = generateRandomColor() // -> #e1ac94
+// // someDiv.style.color = generateRandomColor()
+
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
